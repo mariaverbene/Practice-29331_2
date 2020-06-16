d1ac40af57464d06da8cacf70fe57098692c0867
mariaverbene <62248312+mariaverbene@users.noreply.github.com>
2020-06-16 14:04:22 +0300

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,16 @@
+package kafkaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties(ConfigProperties.class)
+
+public class Demo {
+
+    public static void main(String[] args)  {
+        SpringApplication.run(Demo.class, args);
+    }
+}
+
