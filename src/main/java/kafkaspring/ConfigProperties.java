c56b1bb04882e99ab520e0016a6e56808c505455
mariaverbene <62248312+mariaverbene@users.noreply.github.com>
2020-06-17 14:51:46 +0300

    Add files via upload

--- src/main/java/kafkaspring/ConfigProperties.java
+++ src/main/java/kafkaspring/ConfigProperties.java
@@ -1,11 +1,7 @@
 package kafkaspring;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
-import org.springframework.validation.annotation.Validated;
 
 @Component
 @ConfigurationProperties("config")
