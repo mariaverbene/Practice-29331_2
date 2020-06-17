cb16ba873187a50fd69146daac42e57a1e1a5bf3
mariaverbene <62248312+mariaverbene@users.noreply.github.com>
2020-06-17 14:53:09 +0300

    Update SomeClass.java

--- src/main/java/kafkaspring/SomeClass.java
+++ src/main/java/kafkaspring/SomeClass.java
@@ -1,6 +1,5 @@
 package kafkaspring;
 
-
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
